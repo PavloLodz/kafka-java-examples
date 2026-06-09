c949653a9c0502835aa88b5b8a01b9c4a1aa3d9a
PavloLodz <pavlo.pl20@gmail.com>
2026-06-09 19:24:14 +0200

    Changes in KafkaConsumerConfig

--- SpringBoot_3/kafka-outbox/consumer/src/main/java/pl/ldz/kafka/consumer/config/KafkaConsumerConfig.java
+++ SpringBoot_3/kafka-outbox/consumer/src/main/java/pl/ldz/kafka/consumer/config/KafkaConsumerConfig.java
@@ -1,17 +1,22 @@
 package pl.ldz.kafka.consumer.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.springframework.util.backoff.FixedBackOff;
 import pl.ldz.kafka.common.dto.OrderEventDto;
 
@@ -23,6 +28,21 @@ public class KafkaConsumerConfig {
 
   @Value("${spring.kafka.bootstrap-servers}")
   private String bootstrapServers;
+
+  @Bean
+  public ProducerFactory<String, OrderEventDto> producerFactory() {
+    Map<String, Object> props = new HashMap<>();
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+    return new DefaultKafkaProducerFactory<>(props);
+  }
+
+  @Bean
+  public KafkaTemplate<String, OrderEventDto> kafkaTemplate(
+      ProducerFactory<String, OrderEventDto> producerFactory) {
+    return new KafkaTemplate<>(producerFactory);
+  }
 
   @Bean
   public ConsumerFactory<String, OrderEventDto> consumerFactory() {
